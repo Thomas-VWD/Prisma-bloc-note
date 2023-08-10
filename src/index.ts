7f96f94cc0c015bc4df4226eb88dbf79e4f3dda7
Thomas-VWD <thomas.verwaerde@gmail.com>
2023-08-10 12:45:16 +0200

    add table Note routes

--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,10 @@ const app = express();
 
 app.use(express.json());
 
-// ... (Autres routes)
+// ... users routes ... //
 
 app.post(`/signup`, async (req, res) => {
   const { name, email, password } = req.body;
-
   const result = await prisma.user.create({
     data: {
       name,
@@ -21,7 +20,126 @@ app.post(`/signup`, async (req, res) => {
   res.json(result);
 });
 
-// ... (Autres routes)
+app.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!user) {
+      res.status(404).json({ error: `User with ID ${id} not found` });
+      return;
+    }
+    res.json(user);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the user" });
+  }
+});
+
+app.delete("/user/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const user = await prisma.user.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred while deleting the user with ID ${id}`,
+    });
+  }
+});
+
+// ... Note Routes ... //
+
+app.post(`/notes`, async (req, res) => {
+  const { title, content } = req.body;
+  try {
+    const result = await prisma.note.create({
+      data: {
+        title,
+        content,
+      },
+    });
+    res.json(result);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while creating the note" });
+  }
+});
+
+app.get(`/notes`, async (req, res) => {
+  try {
+    const notes = await prisma.note.findMany();
+    res.json(notes);
+  } catch (error) {
+    res.status(500).json({ error: "An error occurred while fetching notes" });
+  }
+});
+
+app.get("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const note = await prisma.note.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!note) {
+      res.status(404).json({ error: `Note with ID ${id} not found` });
+      return;
+    }
+    res.json(note);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the note" });
+  }
+});
+
+app.put("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  const { title, content } = req.body;
+  try {
+    const updatedNote = await prisma.note.update({
+      where: {
+        id: Number(id),
+      },
+      data: {
+        title,
+        content,
+      },
+    });
+    res.json(updatedNote);
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred while updating the note with ID ${id}`,
+    });
+  }
+});
+
+app.delete("/notes/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const deletedNote = await prisma.note.delete({
+      where: {
+        id: Number(id),
+      },
+    });
+    res.json(deletedNote);
+  } catch (error) {
+    res.status(500).json({
+      error: `An error occurred while deleting the note with ID ${id}`,
+    });
+  }
+});
 
 const server = app.listen(3000, () =>
   console.log(`
